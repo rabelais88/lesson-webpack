72afc328c3b8177bd12ccb438d4480e44a932041
sungryeol <sungryeolp@gmail.com>
2021-01-27 19:41:04 +0900

    :sparkles: resolved webpack

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+import lodash from "lodash";
+
+console.log(lodash);
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
 
   return element;
 }
